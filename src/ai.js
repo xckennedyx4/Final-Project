906cfdcea07226708e0311b500d2efef76d5485f
xckennedyx4 <xckennedyx@gmail.com>
2016-04-13 14:17:05 -0700

    gh-pages

--- src/ai.js
+++
@@ -1,9 +0,0 @@
-var AI = {
-  show: function(){
-    var mu = "<div class='fadeBox'> <div class='spinner'> </div> </div>";
-    document.body.innerHTML += mu;
-  },
-  hide: function(){
-    $(".fadeBox").remove();
-  }
-}
